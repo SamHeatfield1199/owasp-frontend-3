eba47bc8417714dc324a84e2375d53e13a0bd7b3
SamHeatfield1199 <asmirnova1199@gmail.com>
2021-05-25 17:33:50 +1000

    init commit

---
+++ src/components/pages/sign-in.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Input from '../utils/form/input';
+import { Formik, Form, Field } from 'formik';
+import useAsyncAction from '../../hooks/use-async-action/use-async-action';
+import Textarea from '../utils/form/textarea';
+import { Rating } from '@material-ui/lab';
+
+
+
+const SignIn = ({ setAlerts, setAccessToken }) => {
+  const [asyncAction] = useAsyncAction();
+
+  return (
+    <Formik
+      initialValues={{ username: 'formik', review: '', rating: 2 }}
+
+      onSubmit={async (values) => {
+        console.log(values)
+        // async
+        const config = {
+          method: 'post',
+          url: `/auth/sign-in`,
+          data: { ...values },
+        };
+
+        const data = await asyncAction(config, setAlerts);
+
+        if (data) setAccessToken(data.accessToken);
+      }}>
+
+      {({ isSubmitting }) => (
+
+        <Form className="sign-in">
+          <h1 className="sign-in__heading">Review</h1>
+          <img className="sign-in__img mb-m-1" src ="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Geneva_MotorShow_2013_-_Lamborghini_Veneno_1.jpg/1280px-Geneva_MotorShow_2013_-_Lamborghini_Veneno_1.jpg"/>
+          <div className="label mt-s-1">Rating</div>
+          <Rating
+            name="rating"
+            className="mb-m-1"
+            size="large"
+          />
+
+          <div className="label mb-s-1">Username</div>
+          <Input className="sign-in__input input mb-m-1" type="text" name="username" />
+
+          <div className="label mb-s-1">Feedback</div>
+          <Textarea className="sign-in__input textarea" type="text" name="review" />
+
+          <button
+            disabled={isSubmitting}
+            type="submit"
+            className={`sign-in__btn btn btn--primary btn--flat 
+            ${isSubmitting ? 'btn--spinner btn--submitted' : ''} mt-m-2`}>
+            Send
+          </button>
+        </Form>
+
+      )}
+    </Formik>
+  );
+};
+
+export default SignIn;
