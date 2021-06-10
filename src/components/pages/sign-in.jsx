6d0ec02a60626b2ffc61159e931345c114199157
SamHeatfield1199 <asmirnova1199@gmail.com>
2021-06-10 20:20:12 +1000

    :)

--- src/components/pages/sign-in.jsx
+++ src/components/pages/sign-in.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../utils/form/input';
 import { Formik, Form, Field } from 'formik';
 import useAsyncAction from '../../hooks/use-async-action/use-async-action';
@@ -9,13 +9,16 @@ import { Rating } from '@material-ui/lab';
 
 const SignIn = ({ setAlerts, setAccessToken }) => {
   const [asyncAction] = useAsyncAction();
+  const [review, setReview] = useState()
 
   return (
-    <Formik
-      initialValues={{ username: 'formik', review: '', rating: 2 }}
 
+    <Formik
+      initialValues={{ username: '', review: '', rating: 2 }}
       onSubmit={async (values) => {
-        console.log(values)
+        console.log(typeof (values))
+        setReview(values)
+        console.log(review)
         // async
         const config = {
           method: 'post',
@@ -23,18 +26,23 @@ const SignIn = ({ setAlerts, setAccessToken }) => {
           data: { ...values },
         };
 
-        const data = await asyncAction(config, setAlerts);
+        // const data = await asyncAction(config, setAlerts);
 
-        if (data) setAccessToken(data.accessToken);
+        // if (data) setAccessToken(data.accessToken);
       }}>
 
-      {({ isSubmitting }) => (
+      {({ isSubmitting, setFieldValue }) => (
+
 
         <Form className="sign-in">
+          <div>{review}</div>
           <h1 className="sign-in__heading">Review</h1>
-          <img className="sign-in__img mb-m-1" src ="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Geneva_MotorShow_2013_-_Lamborghini_Veneno_1.jpg/1280px-Geneva_MotorShow_2013_-_Lamborghini_Veneno_1.jpg"/>
-          <div className="label mt-s-1">Rating</div>
+          <img className="sign-in__img mb-m-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Geneva_MotorShow_2013_-_Lamborghini_Veneno_1.jpg/1280px-Geneva_MotorShow_2013_-_Lamborghini_Veneno_1.jpg" />
+          <div onMouseEnter={() => { }} onMouseLeave={() => { }} className="label mt-s-1">Rating</div>
           <Rating
+            onChange={(event, newValue) => {
+              setFieldValue("rating", newValue);
+            }}
             name="rating"
             className="mb-m-1"
             size="large"
